9b072691ab3fa0b94f545889df4ee7743e6dbcea
PPSh-1895 <89904972+PPSh-1895@users.noreply.github.com>
2021-11-13 23:00:30 +0800

    Update DrugMapper.java

--- src/main/java/com/se/hospitalmanagement/mapper/DrugMapper.java
+++ src/main/java/com/se/hospitalmanagement/mapper/DrugMapper.java
@@ -40,9 +40,25 @@ public interface DrugMapper extends MyMapper<Drug> {
     })
     List<Drug> selectAllDrugs();
 
-
     /** change the stock number of a particular drug **/
     @Update("update t_drug set stock=#{new_stock} where drug_id=#{drug_id}")
     int Update_Stock(@Param("new_stock") int new_stock, @Param("drug_id") int drug_id);
+
+    @Select("select count(*) from t_drug where drug_name=#{drug_name}")
+    int countByDrugName(@Param("drug_name") String drug_name);
+
+    @Select("select * from t_drug where drug_name=#{drug_name}")
+    @Results({
+            @Result(id = true, column = "drug_id", property = "drug_id"),
+            @Result(column = "drug_name", property = "drug_name"),
+            @Result(column = "drug_producer", property = "drug_producer"),
+            @Result(column = "drug_produceDate", property = "drug_produceDate"),
+            @Result(column = "drug_expdate", property = "drug_expdate"),
+            @Result(column = "stock", property = "stock"),
+            @Result(column = "drug_abstract", property = "drug_abstract"),
+            @Result(column = "drug_description", property = "drug_description"),
+            @Result(column = "commen", property = "commen")
+    })
+    Drug selectByDrugName(@Param("drug_name") String drug_name);
 
 }
