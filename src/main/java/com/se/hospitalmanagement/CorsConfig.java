f5c9d13c72fab9b7fe29e9164c22cdce3e870a91
PPSh-1895 <89904972+PPSh-1895@users.noreply.github.com>
2021-11-13 17:56:12 +0800

    Update CorsConfig.java

--- src/main/java/com/se/hospitalmanagement/CorsConfig.java
+++ src/main/java/com/se/hospitalmanagement/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.lin.appapidemo;
+package com.se.hospitalmanagement;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
